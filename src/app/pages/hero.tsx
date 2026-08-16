9ed71cf0c67a9535ee914f7921e250ebbe8f3aea
devin gupta <tsunamidev1@gmail.com>
2026-08-16 09:10:28 -0700

    updated edu and hero with nikki bug

--- src/app/pages/hero.tsx
+++ src/app/pages/hero.tsx
@@ -49,17 +49,10 @@ export default function Hero() {
             </div>
           </div>
         </div>
-
-        {/* 
-           I'm deeply curious about "people systems"; the frameworks that govern how we interact and exchange with one another like the internet/journalism, financial markets, our political infrastructure, etc... 
-
-        
-        */}
-
         <p id='extended bio' className='paragraph'> I'm deeply curious about "people systems"; the frameworks that govern how we interact and exchange with one another like the internet/<a href="https://elestoque.org/staff_name/devin-gupta/" target="_blank">journalism</a>, financial markets, our <a href="https://rain.stanford.edu/" target="_blank">political infrastructure</a> or art. 
           Often, I've had the most fun exploring these systems with machine learning and interactive visualizations.
           <br /> <br />
-          In my free time, I'm bit fan of puzzling, <a href="https://escaperoom.stanford.edu/" target="_blank">escaping rooms</a>, board games, following <a href="https://www.pi.website/blog" target="_blank">generalizable robotics</a>, hiking Castle Rock, listening to <a href="https://open.spotify.com/show/1te7oSFyRVekxMBJUSethH?si=92bc321c89c54098" target="_blank">Joe and Tracy</a> or visiting friends in SF.
+          In my free time, I'm a big fan of puzzling, <a href="https://escaperoom.stanford.edu/" target="_blank">escaping rooms</a>, board games, following <a href="https://www.pi.website/blog" target="_blank">generalizable robotics</a>, hiking Castle Rock, listening to <a href="https://open.spotify.com/show/1te7oSFyRVekxMBJUSethH?si=92bc321c89c54098" target="_blank">Joe and Tracy</a> or visiting friends in SF.
           <br /> <br />
           My favorite way to spend time is to meet new people, please don't hesitate to reach out. Send me an email <a href="mailto:devgupta@stanford.edu" target="_blank">here</a>! </p>
       </div>
